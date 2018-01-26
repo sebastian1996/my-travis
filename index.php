13a6ebc85a5cd9539ed9d83e670b362763456cb5
sebastian1996 <sebastiangonzalez28@hotmail.com>
2018-01-26 16:15:16 -0500

    agrego css

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php  
-echo "hola mundo cruel de Heroku";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Titulo</title>
+	<link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
+	<script type="text/javascript" src="vendor/twbs/bootstrap/assets/js/vendor/jquery-slim.min.js"></script>
+	<script type="text/javascript" src="vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
+</head>
+<body>
+	<button type="button" class="btn btn-primary">Primary</button>
+</body>
+</html>
