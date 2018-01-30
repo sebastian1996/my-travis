a6b3abb7a8d07977a480ebfc7a13b6a4c20c5f90
sebastian1996 <sebastiangonzalez28@hotmail.com>
2018-01-30 09:36:38 -0500

    new project

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 	<title>Titulo</title>
-	<link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
-	<script type="text/javascript" src="vendor/twbs/bootstrap/assets/js/vendor/jquery-slim.min.js"></script>
-	<script type="text/javascript" src="vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
+	<link rel="stylesheet" type="text/css" href="components/bootstrap/dist/css/bootstrap.css">
+	<script type="text/javascript" src="components/jquery/jquery.min.js"></script>
+	<script type="text/javascript" src="components/bootstrap/dist/js/bootstrap.js"></script>
 </head>
 <body>
 	<button type="button" class="btn btn-primary">Primary</button>
